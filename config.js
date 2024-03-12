6531004fff53afa78773fb0de1315accbaa052ef
tekkasit <tekkasit@gmail.com>
2024-03-12 10:16:27 +0800

    Initial commit

---
+++ config.js
@@ -0,0 +1,8 @@
+// config.js
+module.exports = {
+    mongoURI: 'mongodb+srv://username:password@demo.clustername.mongodb.net/',
+    databaseName: 'sample_mflix', // Replace with your database name
+    collectionName: 'embedded_movies', // Replace with your collection name
+  };
+  
+  
